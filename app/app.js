2802ef2d78e18631c2fb630145e1cb8512d09b4c
renzo <ralvarezrenzo@gmail.com>
2021-06-25 00:21:28 -0500

    prueba header

--- app/app.js
+++ app/app.js
@@ -1,1 +1,6 @@
-alert('Probando')
+const d = document;
+const btnMenu = d.getElementById('btn-hamburger');
+const menu = d.querySelector('.menu')
+btnMenu.addEventListener('click',()=>{
+    menu.classList.toggle('cambiaso')
+})
