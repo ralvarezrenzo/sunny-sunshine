2f07386060c9768017eb857ed8560f3e09f024ab
renzo <ralvarezrenzo@gmail.com>
2021-06-27 11:38:54 -0500

    btnMenu update

--- app/events/navbar.js
+++ app/events/navbar.js
@@ -3,6 +3,9 @@ export default function navBar(btnMenu,menu){
     d.addEventListener('click',(e)=>{
         if(e.target.matches(btnMenu)||e.target.matches(`${btnMenu} *`)){
             d.querySelector(menu).classList.toggle('menu-active')
+        }else{
+            // d.querySelector(menu).
+            d.querySelector(menu).classList.remove('menu-active')
         }
     })
 }
